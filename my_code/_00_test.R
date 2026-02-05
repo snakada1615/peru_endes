f065d55c74d43bb3eb199f4dc1007a30540b300f
snakada1615 <khf04440@nifty.com>
2026-02-05 22:38:29 +0900

    _test.r: 変数変換のための事前チェック

--- my_code/_00_test.R
+++ my_code/_00_test.R
@@ -1,40 +1,92 @@
-# ★★★ 診断コード1: 年度ごとのラベル定義を確認 ★★★
-for (yr in yearlist) {
-  root_folder <- file.path(gdrive_dir, "output", yr) %>%
-    normalizePath() %>%
-    trimws()
-  
-  year_data <- read_rds(file.path(root_folder, "data_merged.rds")) %>%
-    filter(!is.na(hhid))
-  
-  cat("========================================\n")
-  cat("Year:", yr, "\n")
-  cat("nt_wm_ht value labels:\n")
-  print(attr(year_data$nt_wm_ht, "labels"))
-  cat("nt_wm_ht クラス:", class(year_data$nt_wm_ht), "\n")
-  cat("nt_wm_ht table (元データ):\n")
-  print(table(year_data$nt_wm_ht, useNA = "always"))
-  cat("\n")
-}
+library(survey)
+library(srvyr)
+library(ggplot2)
+library(dplyr)
+library(haven)
+library(tidyr)
 
-# ★★★ 診断コード2: rbindlist直後の状態確認 ★★★
-all_data_merged <- rbindlist(data_list, fill = TRUE, ignore.attr = TRUE)
-all_data_merged <- as_tibble(all_data_merged)
+# オブジェクトクリア
+rm(list = ls(all = TRUE))
 
-cat("rbindlist直後のnt_wm_ht (2008年のみ):\n")
-print(table(all_data_merged %>% filter(year == "2008") %>% pull(nt_wm_ht), useNA = "always"))
+# DHSデータのルートフォルダを指定
+gdrive_dir <- "/Users/snakada/Library/CloudStorage/GoogleDrive-snakada@g.ecc.u-tokyo.ac.jp/マイドライブ/Peru_work/Peru_endes/spss"
 
-# ★★★ 診断コード3: restore_labels直後の状態確認 ★★★
-all_data_merged <- restore_labels(all_data_merged, label_merged_all)
+# 結果保存先
+save_path <- file.path(gdrive_dir, "output")
 
-cat("restore_labels直後のnt_wm_ht (2008年のみ):\n")
-print(table(all_data_merged %>% filter(year == "2008") %>% pull(nt_wm_ht), useNA = "always"))
-cat("nt_wm_ht labels:\n")
-print(attr(all_data_merged$nt_wm_ht, "labels"))
+source("myTools.R")
+source("my_code/_tool_SaveLabel.R")
 
-# ★★★ 診断コード4: to_factor直後の状態確認 ★★★
-all_data_merged <- all_data_merged %>%
-  mutate(across(where(is.labelled), labelled::to_factor))
+# ステップ1: 元データの読み込み
+df_org <- readRDS(
+  file.path(gdrive_dir, "output","all_data_merged.rds")
+) %>%
+  filter(!is.na(v021) & !is.na(v022) & !is.na(sampling_weight_trimmed))
 
-cat("to_factor直後のnt_wm_ht (2008年のみ):\n")
-print(table(all_data_merged %>% filter(year == "2008") %>% pull(nt_wm_ht), useNA = "always"))
+# 変数ラベルをメモリに保存したうえでラベル消去
+labels_memory <- save_labels_to_memory(df_org)
+df_org <- remove_labels(df_org)
+
+# キーテーブルの作成
+key_table <- distinct(
+  df_org,
+  year,
+  state,
+  treatment_group,
+  treated_status,
+  year_treated,
+  treatment_start,
+  treatment_group,
+  analysis_grp,
+  group_treated
+)
+
+# 分析に利用する変数名の抽出
+variable_for_analysis <- names(df_org)
+variable_for_analysis <- variable_for_analysis[grepl("^[a-zA-Z]{2}_|^enaho_|^juntos_", variable_for_analysis)]
+
+# === ファクター型を数値に変換 + NaN/Inf対策 ===
+# df_org <- df_org %>%
+#   mutate(
+#     across(
+#       all_of(variable_for_analysis),
+#       ~ {
+#         result <- if (is.factor(.)) {
+#           as.numeric(.) - 1
+#         } else if (is.numeric(.)) {
+#           .
+#         } else if (is.character(.)) {
+#           as.numeric(.)
+#         } else if (is.logical(.)) {
+#           as.numeric(.)
+#         } else {
+#           as.numeric(as.character(.))
+#         }
+#         
+#         # NaN と Inf を NA に変換
+#         result[is.nan(result) | is.infinite(result)] <- NA
+#         result
+#       }
+#     )
+#   )
+
+df_vars <- df_org %>%
+  select(
+    all_of(c("year", "state", "treatment_group", "treated_status", "year_treated", "treatment_start", "treatment_group", "analysis_grp", "group_treated")),
+    all_of(variable_for_analysis)
+  ) %>% slice_head()
+df_vars <- restore_labels(df_vars, labels_memory)
+
+var_summary <- sapply(names(df_vars), function(x) {
+  res <- list(
+    is_factor = is.factor(df_vars[[x]]),
+    is_character = is.character(df_vars[[x]]),
+    is_logical = is.logical(df_vars[[x]]),
+    is_numeric = is.numeric(df_vars[[x]]),
+    factor_levels = levels(df_vars[[x]]),
+    value_labels = paste(attr(df_vars[[x]], "labels"), "_")
+  )
+  return(res)
+})
+
+
